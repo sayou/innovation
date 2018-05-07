641b21e6a39e3432a789540053a294bd5c2fa1de
Mayzrane-Hassan <mayzranehassan@gmail.com>
2018-05-07 19:51:08 +0200

    mise en place I

---
+++ views/header.php
@@ -0,0 +1,12 @@
+<div id="logo_home">
+    <h1><a href="index.html" title="Quote">Quote - Request a quote for every type of companies</a></h1>
+</div>
+
+<a id="menu-button-mobile" class="cmn-toggle-switch cmn-toggle-switch__htx" href="javascript:void(0);"><span>Menu mobile</span></a>
+<nav class="main_nav">
+    <ul class="nav nav-tabs">
+        <li><a href="#tab_1" data-toggle="tab">Inscription</a></li>
+        <li><a href="#tab_2" data-toggle="tab">About</a></li>
+        <li><a href="#tab_3" data-toggle="tab">Contact</a></li>
+    </ul>
+</nav>
---
+++ views/inscription.php
@@ -0,0 +1,203 @@
+                        <div class="row">
+                            <aside class="col-xl-3 col-lg-4">
+                                <h2>Request a Quote and Compare prices!</h2>
+                                <p class="lead">An mei sadipscing dissentiet, eos ea partem viderer facilisi.</p>
+                                <ul class="list_ok">
+                                    <li>Delicata persecuti ei nec, et his minim omnium, aperiam placerat ea vis.</li>
+                                    <li>Suavitate vituperatoribus pro ad, cum in quis propriae abhorreant.</li>
+                                    <li>Aperiri deterruisset ei mea, sed cu laudem intellegat, eu mutat iuvaret voluptatum mei.</li>
+                                </ul>
+                            </aside><!-- /aside -->
+
+                            <div class="col-xl-9 col-lg-8">
+                                <div id="wizard_container">
+                                    <div id="top-wizard">
+                                        <strong>Progress</strong>
+                                        <div id="progressbar"></div>
+                                    </div><!-- /top-wizard -->
+
+                                    <form name="example-1" id="wrapped" method="POST" enctype="multipart/form-data">
+                                        <input id="website" name="website" type="text" value=""><!-- Leave for security protection, read docs for details -->
+                                        <div id="middle-wizard">
+                                            <div class="step">
+                                                <h3 class="main_question"><strong>1/4</strong>What is your budget?</h3>
+                                                
+                                                <div class="form-group radio_questions">
+                                                    <label>1. My budget is under $500
+                                                        <input name="question_1" type="radio" value="My budget is under $500" class="icheck required">
+                                                    </label>
+                                                </div>
+                                                <div class="form-group radio_questions">
+                                                    <label>2. My budget is between $500 and $1000
+                                                        <input name="question_1" type="radio" value="My budget is between $500 and $1000" class="icheck required">
+                                                    </label>
+                                                </div>
+                                                <div class="form-group radio_questions">
+                                                    <label>3. My budget is between $1000 and $1500
+                                                        <input name="question_1" type="radio" value="My budget is between $1000 and $1500" class="icheck required">
+                                                    </label>
+                                                </div>
+                                                <div class="form-group radio_questions">
+                                                    <label>4. My budget is over $1500
+                                                        <input name="question_1" type="radio" value="My budget is over $1500" class="icheck required">
+                                                    </label>
+                                                </div>
+                                                
+                                            </div><!-- /step 1-->
+
+                                            <div class="step">
+                                                <h3 class="main_question"><strong>2/4</strong>What your poject need?</h3>
+                                                <div class="row add_bottom_30">
+                                                   
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group checkbox_questions">
+                                                            <label>
+                                                                <input name="question_2[]" type="checkbox" value="Custom interface and layout" class="icheck required">Custom interface and layout
+                                                            </label>
+                                                        </div>
+                                                        <div class="form-group checkbox_questions">
+                                                            <label>
+                                                                <input name="question_2[]" type="checkbox" value="Web site design" class="icheck required">Web site design
+                                                            </label>
+                                                        </div>
+                                                        <div class="form-group checkbox_questions">
+                                                            <label>
+                                                                <input name="question_2[]" type="checkbox" value="Seo optimization" class="icheck required">Seo optimization
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group checkbox_questions">
+                                                            <label>
+                                                                <input name="question_2[]" type="checkbox" value="CMS integrations (Wordpress)" class="icheck required">CMS integrations (Wordpress)
+                                                            </label>
+                                                        </div>
+                                                        <div class="form-group checkbox_questions">
+                                                            <label>
+                                                                <input name="question_2[]" type="checkbox" value="Domain registration" class="icheck required">Domain registration
+                                                            </label>
+                                                        </div>
+                                                        <div class="form-group checkbox_questions">
+                                                            <label>
+                                                                <input name="question_2[]" type="checkbox" value="Hosting plan" class="icheck required">Hosting plan
+                                                            </label>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </div><!-- /row-->
+                                                <div class="form-group textarea_info">
+                                                    <label>Additional info</label>
+                                                    <textarea name="addtional_info" class="form-control" style="height:150px;" placeholder="How many pages, other details, etc..."></textarea>
+                                                </div>
+												<div class="form-group">
+													<label>Optional file upload<br><small>(Files accepted: gif, jpg, jpeg, .png, .doc, .pdf - Max file size: 50k for demo purpose, you can increase based on your host settings.)</small></label>
+													<div class="fileupload">
+														<input type="file" name="fileupload" accept="image/*,doc,pdf">
+													</div>
+												</div>
+                                            </div><!-- /step 2-->
+
+                                            <div class="step">
+                                                <h3 class="main_question"><strong>3/4</strong>Please answer the following questions:</h3>
+
+                                                <div class="row">
+                                                   
+                                                    <div class="col-lg-10">
+                                                        <div class="form-group select">
+                                                            <label>If you already have an hosting plan, please define:</label>
+                                                            <div class="styled-select">
+                                                                <select class="required" name="select_1">
+                                                                    <option value="" selected>Select</option>
+                                                                    <option value="Unix/Linux + Mysql">Unix/Linux + Mysql</option>
+                                                                    <option value="Windows + Sql">Windows + Sql</option>
+                                                                    <option value="Other">Other</option>
+                                                                </select>
+                                                            </div>
+                                                        </div><!-- /select-->
+
+                                                        <div class="form-group select">
+                                                            <label>If you need an hosting plan, please define which one:</label>
+                                                            <div class="styled-select">
+                                                                <select class="required" name="select_2">
+                                                                    <option value="" selected>Select</option>
+                                                                    <option value="Hosting Plan 1 year + Mysql database 500MB">1 year + Mysql database 500MB</option>
+                                                                    <option value="Hosting Plan 2 year + Mysql database 500MB">2 year + Mysql database 500MB</option>
+                                                                    <option value="Hosting Plan 2 year + Mysql database 1GB">2 year + Mysql database 1GB</option>
+                                                                </select>
+                                                            </div>
+                                                        </div><!-- /select-->
+
+                                                        <div class="form-group select">
+                                                            <label>If you need a newsletter campaign, please define the provider you prefer:</label>
+                                                            <div class="styled-select">
+                                                                <select class="required" name="select_3">
+                                                                    <option value="" selected>Select</option>
+                                                                    <option value="Mailchimp">Mailchimp</option>
+                                                                    <option value="CampaignMonitor">CampaignMonitor</option>
+                                                                    <option value="MailUp">MailUp</option>
+                                                                </select>
+                                                            </div>
+                                                        </div><!-- /select-->
+                                                    </div>
+                                                </div><!-- /row-->
+                                            </div><!-- /step 3-->
+
+                                            <div class="submit step">
+
+                                                <h3 class="main_question"><strong>4/4</strong>Please fill with your details</h3>
+
+                                                <div class="row">
+                                                   
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <input type="text" name="company_name" class="form-control" placeholder="Your company name">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <input type="text" name="firstname" class="required form-control" placeholder="First name">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <input type="text" name="lastname" class="required form-control" placeholder="Last name">
+                                                        </div>
+                                                    </div><!-- /col-sm-6 -->
+
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <input type="email" name="email" class="required form-control" placeholder="Your Email">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <input type="text" name="telephone" class="required form-control" placeholder="Your Telephone">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <div class="styled-select">
+                                                                <select class="required" name="country">
+                                                                    <option value="" selected>Select your country</option>
+                                                                    <option value="Europe">Europe</option>
+                                                                    <option value="Asia">Asia</option>
+                                                                    <option value="North America">North America</option>
+                                                                    <option value="South America">South America</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div><!-- /col-sm-6 -->
+                                                </div><!-- /row -->
+                                                
+                                                <div class="form-group checkbox_questions">
+                                                    <input name="terms" type="checkbox" class="icheck required" value="yes">
+                                                    <label>Please accept <a href="#" data-toggle="modal" data-target="#terms-txt">terms and conditions</a> ?
+                                                    </label>
+                                                </div>
+
+                                            </div><!-- /step 4-->
+
+                                        </div><!-- /middle-wizard -->
+                                        <div id="bottom-wizard">
+                                            <button type="button" name="backward" class="backward">Backward </button>
+                                            <button type="button" name="forward" class="forward">Forward</button>
+                                            <button type="submit" name="process" class="submit">Submit</button>
+                                        </div><!-- /bottom-wizard -->
+                                    </form>
+                                </div><!-- /Wizard container -->
+
+                            </div><!-- /col -->
+                        </div><!-- /row -->
