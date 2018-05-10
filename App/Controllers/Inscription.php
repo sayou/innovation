8ca66826b3e67c2055794217d63462670c505cb6
Mayzrane-Hassan <mayzranehassan@gmail.com>
2018-05-10 03:41:47 +0200

    Inscription step II

--- App/Controllers/Inscription.php
+++ App/Controllers/Inscription.php
@@ -9,14 +9,18 @@ class Inscription extends \Core\Controller{
 
     public function addNewAction(){
 
-    	try {
-    		$idLead = InscriptionModel::addLeadInfos($_POST);
-			$idProjet = InscriptionModel::addProjetInfos($_POST, $idLead);
-			InscriptionModel::addMembresInfos($_POST, $idProjet);
-			$test = true;
-    	} catch (Exception $e) {
-    		$test = false;	
-    	}    	
+    	if(isset($_POST['process'])){
+	    	try {
+	    		$idLead = InscriptionModel::addLeadInfos($_POST);
+				$idProjet = InscriptionModel::addProjetInfos($_POST, $idLead);
+				InscriptionModel::addMembresInfos($_POST, $idProjet);
+				$test = 'true';
+	    	} catch (Exception $e) {
+	    		$test = 'false';	
+	    	}  
+	    }  
+	    else
+	    	$test = null;
 
         View::getView('Home/index.html', ["test" => $test]);   		
     }
